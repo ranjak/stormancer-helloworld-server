0a37a2fb11527f47740b59a34bfc85973e9f656b
ranjak <ranjak.hector@gmail.com>
2017-01-19 16:50:30 +0100

    Initial commit

---
+++ App.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stormancer;
+using Stormancer.Core;
+
+namespace tutorial_helloworld
+{
+    public class App
+    {
+      public void Run(IAppBuilder builder)
+      {
+      builder.SceneTemplate("hello", scene =>
+      {
+        scene.Connected.Add(peer =>
+        {
+          peer.Send("msg", "Hello World!");
+          return Task.FromResult(true);
+        });
+      });
+      }
+    }
+}
